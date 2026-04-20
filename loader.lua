bba9fd4143236d205ccfd57fbd36acd3ac1c0afc
IsotopeHub <277853328+IsotopeHub@users.noreply.github.com>
2026-04-20 20:47:57 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,20 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Isotope Hub | Loader",
+   LoadingTitle = "Isotope Hub Rendszer",
+   LoadingSubtitle = "Verzió: 1.0",
+   KeySystem = true,
+   KeySettings = {
+      Title = "Isotope Key System",
+      Subtitle = "Szerezz kulcsot: work.ink/pelda",
+      Note = "A kulcs naponta változik!",
+      FileName = "IsotopeKey",
+      SaveKey = true,
+      Key = {"TESZT_KULCS_123"}
+   }
+})
+
+local placeId = game.PlaceId
+
+Rayfield:Notify({Title = "Error", Content = "This game is not supported by Isotope Hub."})
