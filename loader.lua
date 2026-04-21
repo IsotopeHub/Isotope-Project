cfd5733d3ca80c258de051b1e85f1badb839683a
IsotopeHub <277853328+IsotopeHub@users.noreply.github.com>
2026-04-21 17:36:19 +0200

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,3 @@
-loadstring(game:HttpGet("https://keysystem.work.ink/scripts/019dac60-c482-77e9-84a9-bab9f7c8e67c"))()
-
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
