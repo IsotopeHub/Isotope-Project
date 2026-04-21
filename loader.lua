89d103342d4c04dae6f9562bc4d20fe0e9464d1a
IsotopeHub <277853328+IsotopeHub@users.noreply.github.com>
2026-04-21 17:55:42 +0200

    loader.lua

--- loader.lua
+++ loader.lua
@@ -2,10 +2,43 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Isotope Hub",
-   LoadingTitle = "Magic Happenings",
-   KeySystem = false,
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Magic Hapennings",
+   LoadingSubtitle = "by gerixycs",
+   ShowText = "Rayfield", -- for mobile users to unhide Rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from emitting warnings when the script has a version mismatch with the interface.
+
+   -- ScriptID = "sid_xxxxxxxxxxxx", -- Your Script ID from developer.sirius.menu — enables analytics, managed keys, and script hosting
+
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "https://discord.gg/zM7a4nENPr", -- The Discord invite code, do not include Discord.gg/. E.g. Discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the Discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Isotope Hub Key System",
+      Subtitle = "Get key to use Isotope Hub",
+      Note = "Link: https://direct-link.net/5245958/D6WFCxoJAP9a", -- Use this to tell the user how to get a key
+      FileName = "IsotopeKey", -- It is recommended to use something unique, as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"release!"} -- List of keys that the system will accept, can be RAW file links (pastebin, github, etc.) or simple strings ("hello", "key22")
+   }
 })
 
 local placeId = game.PlaceId
 
-Rayfield:Notify({Title = "Error", Content = "This game is not supported by Isotope Hub."})
+print("Place ID: " .. placeId)
